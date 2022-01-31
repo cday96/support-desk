2c88f045e8a24da822efdc9d0278cbf093bce651
Carter Day <carter.day96@outlook.com>
2022-01-31 18:54:11 -0500

    add jwt to register and login payloads for auth

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler")
 const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 
 const User = require("../models/userModel")
 
@@ -39,6 +40,7 @@ const registerUser = asyncHandler(async (req, res) => {
 			_id: user._id,
 			name: user.name,
 			email: user.email,
+			token: generateToken(user._id),
 		})
 	} else {
 		res.status(400)
@@ -60,6 +62,7 @@ const loginUser = asyncHandler(async (req, res) => {
 			_id: user._id,
 			name: user.name,
 			email: user.email,
+			token: generateToken(user._id),
 		})
 	} else {
 		res.status(401)
@@ -68,6 +71,13 @@ const loginUser = asyncHandler(async (req, res) => {
 
 	// res.send("login Route")
 })
+
+// Generate JWT
+const generateToken = (id) => {
+	return jwt.sign({ id }, process.env.JWT_SECRET, {
+		expiresIn: "30d",
+	})
+}
 
 module.exports = {
 	registerUser,
